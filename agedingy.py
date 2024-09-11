8afe77410ec02b2727f6c9e45761464e00f9bfd7
ManedHowler <Mike.cantu628@gmail.com>
2024-09-11 08:41:08 -0400

    Add files via upload

---
+++ agedingy.py
@@ -0,0 +1,16 @@
+# This program calculates your age in the year 2050.
+# Input:  None
+# Output: Your current age followed by your age in 2050
+
+# Create your variables here
+
+# insert current age
+myCurrentAge = 20
+
+# insert current year
+currentYear = 2024
+
+# Calculate age in 2050
+myNewAge = myCurrentAge + (2050 - currentYear)
+print("My Current Age is " + str(myCurrentAge))
+print("I will be " + str(myNewAge) + " in 2050.")
